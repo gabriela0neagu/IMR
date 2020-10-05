ae199487d9d0f689c29c8c96a9fb4ccd5afec30a
gabriela0neagu <56290987+gabriela0neagu@users.noreply.github.com>
2020-10-05 21:00:21 +0300

    lab 1

---
+++ Assets/amusedART/CloseUpBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseUpBehaviour : MonoBehaviour
+{
+   public Animator animator;
+   public GameObject AR;
+   public GameObject rabbit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      animator.SetBool("closeUp", false);
+
+   }
+
+   // Update is called once per frame
+   void Update()
+    {
+      float dist = Vector3.Distance(AR.transform.position, rabbit.transform.position);
+      if(dist <= 10)
+      {
+         animator.SetBool("closeUp", true);
+      }
+      else
+      {
+         animator.SetBool("closeUp", false);
+      }
+   }
+}
